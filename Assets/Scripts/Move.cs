960a75b194b9f34381e08fffbf67f0fbea305c60
David Neilsen <david@panmedia.co.nz>
2013-12-15 14:58:11 +1300

    Init

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float moveSpeed = 5;
+
+	public void Start() {
+	
+	}
+	
+	public void Update() {
+		if (Input.GetAxis("Vertical") > 0.02f) {
+			this.rigidbody.AddForce(transform.up * -moveSpeed);
+		} else if (Input.GetAxis("Vertical") < -0.02f) {
+			this.rigidbody.AddForce(transform.up * moveSpeed);
+		}
+		if (Input.GetAxis("Horizontal") > 0.02f) {
+			this.rigidbody.AddForce(transform.right * moveSpeed);
+		} else if (Input.GetAxis("Horizontal") < -0.02f) {
+			this.rigidbody.AddForce(transform.right * -moveSpeed);
+		}
+	}
+
+}
